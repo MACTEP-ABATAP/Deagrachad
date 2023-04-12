0166d1f84b80878851c018dd53f3400f1b9f47e6
macter-abatar <seva_maltsev@mail.ru>
2023-04-12 22:36:25 +0300

    Add json read (not for bot now but for future use)

--- jsonHandler.py
+++ jsonHandler.py
@@ -1,6 +1,6 @@
-#mport json
-#import requests
-#def jsonHand(File_name):
-#    with open(File_name) as File_data:
-#        data = File_data.read()
-#         = json.loads(configs_data, object_hook=complex)
+import json
+def jsonHand(File_name):
+    with open(File_name) as File_data:
+        jsondata = json.load(File_data)
+    return jsondata
+
--- main.py
+++ main.py
@@ -2,15 +2,25 @@
 import discord
 from discord.ext import commands
 
+from jsonHandler import jsonHand
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+bot = commands.Bot(command_prefix="!DG", intents=intents)
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('Конечно крут')
+async def _ping(ctx):
+    await ctx.send('Я есть Деграчад')
+
+@bot.command()
+async def jsonRead(ctx, arg):
+    result = jsonHand(arg)
+    await ctx.send(result)
+
+#@bot.command()
+#async def 
 
 bot.run('MTA1MDgxMTk0NTczNDkxMDAxMg.GUSr-u.9F6rQ6tLYkdiTN3Rg2krBc1zmbacpfZXUoE44M')
